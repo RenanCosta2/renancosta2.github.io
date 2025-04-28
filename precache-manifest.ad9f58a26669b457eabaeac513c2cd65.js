d68948dc392ad9f692e12efee295aa52506931b2
RenanCosta2 <renanleitedacosta@gmail.com>
2025-04-27 22:46:35 -0300

    Updates

---
+++ precache-manifest.ad9f58a26669b457eabaeac513c2cd65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e84bf218f8b6d226bed596c27c1dca80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9915c5941b149030efc7",
+    "url": "/static/css/main.017d4ca6.chunk.css"
+  },
+  {
+    "revision": "fca391b1cf5df1dfdc66",
+    "url": "/static/js/2.a50b4e11.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "9915c5941b149030efc7",
+    "url": "/static/js/main.c6489c00.chunk.js"
+  },
+  {
+    "revision": "49b1760ced1044a52545",
+    "url": "/static/js/runtime~main.e2e3066b.js"
+  }
+]);
