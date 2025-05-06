acb362631220e62ba95727a293bb789b91955d3d
RenanCosta2 <renanleitedacosta@gmail.com>
2025-05-06 11:08:05 -0300

    fix: mudei o router de browser para hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Resume } from './components/Resume';
 import { Portfolio } from './components/Portfolio';
